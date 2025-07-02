ddc825940b4e71136a4868b2cd071ca883f710a4
brayan.loayza <brayan.loayza.leiva@gmail.com>
2025-07-02 16:50:37 -0500

    layout publico creado

---
+++ src/app/vistas/layout-publico/footer-publico/footer-publico.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer-publico',
+  imports: [],
+  templateUrl: './footer-publico.component.html',
+  styleUrl: './footer-publico.component.scss'
+})
+export class FooterPublicoComponent {
+
+}
